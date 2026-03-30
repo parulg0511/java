45ad51db0311a6eb535ece70b893046b7748b909
parulg0511 <parulg0511@gmail.com>
2026-03-29 17:07:21 -0700

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,23 @@
+package Java_Core;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+
+        // Demonstrating escape sequences in meaningful sentences
+        System.out.println("Welcome to the store!\nWe hope you enjoy your shopping experience.");
+        System.out.println("Use\tTAB\tto align text neatly.");
+        System.out.println("This is a backslash: \\");
+        System.out.println("He said, \"Java is fun!\"");
+        System.out.println("It\'s a great day to code!");
+
+        // Receipt-style output using tabs
+        System.out.println("\n---- RECEIPT ----");
+        System.out.println("Item\t\t\tPrice");
+        System.out.println("Coffee\t\t\t Rs. 50");
+        System.out.println("Sandwich\t\t Rs. 120");
+        System.out.println("Cake\t\t\t Rs. 80");
+    }
+}
+
+
